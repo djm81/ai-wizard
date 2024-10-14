625472708214f3ae215c48619e75f3ca5efc1361
Dominikus <dominikus@mbp-dom.fritz.box>
2024-10-14 20:51:22 +0200

    Improve setup.sh and add self-assumerole permission

--- backend/aws-setup/setup.sh
+++ backend/aws-setup/setup.sh
@@ -8,20 +8,55 @@ read -p "Enter your AWS Region (e.g., eu-west-1): " aws_region
 ai_wizard_role_stack="AIWizardDeploymentRoleStack"
 ai_wizard_ecr_stack="AIWizardECRStack"
 
+ai_wizard_role_template="./cf-ai-wizard-deployment-role.yaml"
+ai_wizard_ecr_template="./cf-ai-wizard-private-ecr.yaml"
+
+# Validate template
+echo "Validating template for AI Wizard Deployment Role ..."
+aws cloudformation validate-template --template-body file://$ai_wizard_role_template --no-cli-pager
+if [ $? -ne 0 ]; then
+    echo "CloudFormation template validation failed. Aborting script."
+    exit 1
+fi
+
 # Check if the AI Wizard Role stack exists
 if aws cloudformation describe-stacks --stack-name "$ai_wizard_role_stack" >/dev/null 2>&1; then
-    echo "Updating stack: $ai_wizard_role_stack"
-    aws cloudformation update-stack --stack-name "$ai_wizard_role_stack" --template-body file://./cf-ai-wizard-deployment-role.yaml --parameters ParameterKey=TrustedAccount,ParameterValue="$aws_account_id" --capabilities CAPABILITY_NAMED_IAM --region $aws_region
+    # Get the deployed template
+    deployed_ai_wizard_role_template=$(aws cloudformation get-template --stack-name $ai_wizard_role_stack --query 'TemplateBody' --output text)
+
+    # Compare deployed with local template for changes
+    if ! diff <(echo "$deployed_ai_wizard_role_template") "$ai_wizard_role_template" > /dev/null; then
+        echo "Updating stack: $ai_wizard_role_stack"
+        aws cloudformation update-stack --stack-name "$ai_wizard_role_stack" --template-body file://$ai_wizard_role_template --parameters ParameterKey=TrustedAccount,ParameterValue="$aws_account_id" --capabilities CAPABILITY_NAMED_IAM --region $aws_region
+    else
+        echo "The AI Wizard Deployment Role stack is up to date."
+    fi
 else
     echo "Creating stack: $ai_wizard_role_stack"
-    aws cloudformation create-stack --stack-name "$ai_wizard_role_stack" --template-body file://./cf-ai-wizard-deployment-role.yaml --parameters ParameterKey=TrustedAccount,ParameterValue="$aws_account_id" --capabilities CAPABILITY_NAMED_IAM --region $aws_region
+    aws cloudformation create-stack --stack-name "$ai_wizard_role_stack" --template-body file://$ai_wizard_role_template --parameters ParameterKey=TrustedAccount,ParameterValue="$aws_account_id" --capabilities CAPABILITY_NAMED_IAM --region $aws_region
+fi
+
+# Validate template
+echo "Validating template for AI Wizard ECR ..."
+aws cloudformation validate-template --template-body file://$ai_wizard_ecr_template --no-cli-pager
+if [ $? -ne 0 ]; then
+    echo "CloudFormation template validation failed. Aborting script."
+    exit 1
 fi
 
 # Check if the AI Wizard ECR stack exists
 if aws cloudformation describe-stacks --stack-name "$ai_wizard_ecr_stack" >/dev/null 2>&1; then
-    echo "Updating stack: $ai_wizard_ecr_stack"
-    aws cloudformation update-stack --stack-name "$ai_wizard_ecr_stack" --template-body file://./cf-ai-wizard-private-ecr.yaml --capabilities CAPABILITY_NAMED_IAM --region $aws_region
+    # Get the deployed template
+    deployed_ai_wizard_ecr_template=$(aws cloudformation get-template --stack-name $ai_wizard_ecr_stack --query 'TemplateBody' --output text)
+
+    # Compare deployed with local template for changes
+    if ! diff <(echo "$deployed_ai_wizard_ecr_template") "$ai_wizard_ecr_template" > /dev/null; then
+        echo "Updating stack: $ai_wizard_ecr_stack"
+        aws cloudformation update-stack --stack-name "$ai_wizard_ecr_stack" --template-body file://$ai_wizard_ecr_template --capabilities CAPABILITY_NAMED_IAM --region $aws_region
+    else
+        echo "The AI Wizard ECR stack is up to date."
+    fi
 else
     echo "Creating stack: $ai_wizard_ecr_stack"
-    aws cloudformation create-stack --stack-name "$ai_wizard_ecr_stack" --template-body file://./cf-ai-wizard-private-ecr.yaml --capabilities CAPABILITY_NAMED_IAM --region $aws_region
+    aws cloudformation create-stack --stack-name "$ai_wizard_ecr_stack" --template-body file://$ai_wizard_ecr_template --capabilities CAPABILITY_NAMED_IAM --region $aws_region
 fi
