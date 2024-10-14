5ef07318786299e1ff6812faca1c07881d3ccbe3
Dominikus <dominikus@mbp-dom.fritz.box>
2024-10-14 22:56:29 +0200

    Fix target dependency issues

--- backend/terraform/main.tf
+++ backend/terraform/main.tf
@@ -209,17 +209,27 @@ resource "aws_acm_certificate" "ai_wizard" {
 # Route53 record for ACM validation
 resource "aws_route53_record" "acm_validation" {
   provider = aws.assume_role
-  zone_id  = var.route53_zone_id
-
-  for_each = toset(aws_acm_certificate.ai_wizard.domain_validation_options)
-
-  name     = each.value.resource_record_name
-  type     = each.value.resource_record_type
-  records  = [each.value.resource_record_value]
-  ttl      = 60
+  for_each = {
+    for dvo in aws_acm_certificate.ai_wizard.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
 
   allow_overwrite = true
-  depends_on = [aws_acm_certificate.ai_wizard]
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = var.route53_zone_id
+}
+
+# Certificate validation
+resource "aws_acm_certificate_validation" "ai_wizard" {
+  provider                = aws.assume_role
+  certificate_arn         = aws_acm_certificate.ai_wizard.arn
+  validation_record_fqdns = [for record in aws_route53_record.acm_validation : record.fqdn]
 }
 
 # Application Load Balancer
