17a8bb4453126a32fcf16f5df65f51021036cb85
Dominikus <dominikus@mbp-dom.fritz.box>
2024-10-30 13:42:09 +0100

    Add frontend pipeline

--- backend/terraform/main.tf
+++ backend/terraform/main.tf
@@ -223,11 +223,13 @@ resource "aws_cloudfront_origin_access_identity" "frontend" {
 resource "aws_cloudfront_distribution" "frontend" {
   provider = aws.assume_role
   origin {
-    domain_name = aws_s3_bucket.frontend.bucket_regional_domain_name
+    domain_name = aws_s3_bucket_website_configuration.frontend.website_endpoint
     origin_id   = "S3-${aws_s3_bucket.frontend.id}"
-
-    s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.frontend.cloudfront_access_identity_path
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
     }
   }
 
