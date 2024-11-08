a2f738a226dd54098726bc82e2f86bb9715cb783
Dominikus <dominikus@mbp-dom.fritz.box>
2024-11-08 13:00:53 +0100

    Fix validation error message

--- backend/terraform/variables.tf
+++ backend/terraform/variables.tf
@@ -23,7 +23,7 @@ variable "environment" {
   type        = string
   validation {
     condition     = contains(["dev", "test", "prod"], var.environment)
-    error_message = "Environment must be one of: dev, test, prod"
+    error_message = "Environment must be one of: dev, test, prod."
   }
 }
 
